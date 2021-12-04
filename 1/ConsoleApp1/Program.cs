626bc3d15e569de32d054bad528f5048f71153b3
SergiyLapiuk <72142067+SergiyLapiuk@users.noreply.github.com>
2021-12-04 23:02:03 +0200

    dz7

---
+++ 1/ConsoleApp1/Program.cs
@@ -0,0 +1,55 @@
+﻿//Порушується принцип єдиного обов'язку
+using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        class Item
+        {
+        }
+        class Order
+        {
+            private List<Item> itemList;
+            internal List<Item> ItemList
+            {
+                get
+                {
+                    return itemList;
+                }
+                set
+                {
+                    itemList = value;
+                }
+            }
+        }
+
+        class OrderItems : Order
+        {
+            public void CalculateTotalSum() {/*...*/}
+            public void GetItems() {/*...*/}
+            public void GetItemCount() {/*...*/}
+            public void AddItem(Item item) {/*...*/}
+            public void DeleteItem(Item item) {/*...*/}
+        }
+
+        class OrderPrintShow : Order
+        {
+            public void PrintOrder() {/*...*/}
+            public void ShowOrder() {/*...*/}
+        }
+
+        class ActionsWithTheOrder : Order
+        {
+            public void Load() {/*...*/}
+            public void Save() {/*...*/}
+            public void Update() {/*...*/}
+            public void Delete() {/*...*/}
+        }
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ 4/ConsoleApp1/Program.cs
@@ -0,0 +1,58 @@
+﻿/* Даний інтерфейс поганий тим, що він включає занадто багато
+методів. Що, якщо наш клас товарів не може мати знижок або
+промокодом, або для нього немає сенсу встановлювати матеріал з якого
+зроблений (наприклад, для книг). Таким чином, щоб не реалізовувати в
+кожному класі невикористовувані в ньому методи, краще розбити
+інтерфейс на кілька дрібних і кожним конкретним класом реалізовувати
+потрібні інтерфейси.*/
+// Порушується принцип розділення інтерфейсу
+using System;
+
+namespace ConsoleApp1
+{
+    interface IApplyDiscount
+    {
+        void ApplyDiscount(String discount);
+    }
+
+    interface IApplyPromocode
+    {
+        void ApplyPromocode(String promocode);
+    }
+    interface ISetColor
+    {
+        void SetColor(byte color);
+    }
+    interface ISetSize
+    {
+        void SetSize(byte size);
+    }
+
+    interface ISetPrice
+    {
+        void SetPrice(double price);
+    }
+
+    class Book: IApplyDiscount, IApplyPromocode, ISetPrice
+    {
+        void IApplyPromocode.ApplyPromocode(String promocode) { }
+        void IApplyDiscount.ApplyDiscount(String discount) { }
+        void ISetPrice.SetPrice(double price) { }
+    }
+
+    class Outerwear: IApplyDiscount, IApplyPromocode, ISetPrice, ISetSize, ISetColor
+    {
+        void IApplyPromocode.ApplyPromocode(String promocode) { }
+        void IApplyDiscount.ApplyDiscount(String discount) { }
+        void ISetPrice.SetPrice(double price) { }
+        void ISetSize.SetSize(byte size) { }
+        void ISetColor.SetColor(byte color) { }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+        }
+    }
+}
